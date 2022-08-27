927d55ca13be41038f7a6bc5bfb56d2212858d87
Tara Pogančev <tarapogancev@gmail.com>
2022-08-27 20:30:40 +0200

    feat: roommate apartment fragments working

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# android-roommates-app
-Android application for finding roommates and apartments.
+# Roommates 
+
+## Android Application for finding Roommates and Apartments
+
+Android client side application for finding roommates and apartments. 
+
+## Resources
+https://www.flaticon.com/packs/essential-set-2
+https://unsplash.com/
+https://themeisle.com/illustrations/
+https://mixkit.co/
+
+## Screenshots
+![Roommates](Screenshots/1.jpg)
+![Roommates](Screenshots/2.jpg)
+![Roommates](Screenshots/3.jpg)
--- Roommates/app/src/main/java/com/tarapogancev/roommates/activity/MainActivity.java
+++ Roommates/app/src/main/java/com/tarapogancev/roommates/activity/MainActivity.java
@@ -1,19 +1,100 @@
 package com.tarapogancev.roommates.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.tarapogancev.roommates.R;
+import com.tarapogancev.roommates.fragment.ApartmentsFragment;
+import com.tarapogancev.roommates.fragment.RoommatesFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    PagerAdapter pagerAdapter;
+    Button roommateButton, apartmentButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, RoommatesActivity.class));
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new RoommatesFragment());
+        fragmentList.add(new ApartmentsFragment());
+
+        viewPager = findViewById(R.id.layout_ViewPager);
+        pagerAdapter = new MainPagerAdapter(getSupportFragmentManager(), fragmentList);
+        viewPager.setAdapter(pagerAdapter);
+
+        roommateButton = findViewById(R.id.button_roommates);
+        apartmentButton = findViewById(R.id.button_apartments);
+
+        roommateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem() != 0) {
+                    viewPager.setCurrentItem(0);
+                    makeButtonBlue(roommateButton);
+                    makeButtonGray(apartmentButton);
+                }
+            }
+        });
+
+        apartmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewPager.getCurrentItem() != 1) {
+                    viewPager.setCurrentItem(1);
+                    makeButtonBlue(apartmentButton);
+                    makeButtonGray(roommateButton);
+                }
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    makeButtonBlue(roommateButton);
+                    makeButtonGray(apartmentButton);
+                } else {
+                    makeButtonBlue(apartmentButton);
+                    makeButtonGray(roommateButton);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void makeButtonBlue(Button button) {
+        button.setBackground(ContextCompat.getDrawable(this, R.drawable.blue_rectangle_round));
+        button.setTextColor(button.getContext().getResources().getColor(R.color.white));
+    }
+
+    private void makeButtonGray(Button button) {
+        button.setBackground(ContextCompat.getDrawable(this, R.drawable.light_gray_rectangle_round));
+        button.setTextColor(button.getContext().getResources().getColor(R.color.gray_medium));
     }
 }
