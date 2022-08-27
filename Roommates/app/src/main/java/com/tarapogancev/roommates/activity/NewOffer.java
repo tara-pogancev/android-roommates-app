7293058a515e2caa59e022bb5841baca82f7e4ca
Tara Pogančev <tarapogancev@gmail.com>
2022-08-27 12:17:38 +0200

    feat: initial commit

---
+++ Roommates/app/src/main/java/com/tarapogancev/roommates/activity/NewOffer.java
@@ -0,0 +1,16 @@
+package com.tarapogancev.roommates.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tarapogancev.roommates.R;
+
+public class NewOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_offer);
+    }
+}
